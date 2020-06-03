96eaae5955484a69613614a737c3f27c2c4a7bc6
Pranav Wadhwa <pranavwadhwa@outlook.com>
2020-06-02 22:27:51 -0400

    Initial setup

---
+++ PWSwipeView/PWSwipeView.h
@@ -0,0 +1,19 @@
+//
+//  PWSwipeView.h
+//  PWSwipeView
+//
+//  Created by Pranav Wadhwa on 6/2/20.
+//  Copyright © 2020 Pranav Wadhwa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PWSwipeView.
+FOUNDATION_EXPORT double PWSwipeViewVersionNumber;
+
+//! Project version string for PWSwipeView.
+FOUNDATION_EXPORT const unsigned char PWSwipeViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PWSwipeView/PublicHeader.h>
+
+
